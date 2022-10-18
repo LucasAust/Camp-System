422350a10aa77078a7cb517cad91ff815ea408fa
LucasAust <89277576+LucasAust@users.noreply.github.com>
2022-10-18 14:25:15 -0400

    edits

--- emergencyContact.java
+++ emergencyContact.java
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> DeepNPatel
 public class emergencyContact {
     private String name;
     private String relationship;
     private String phoneNumber;
 
     public emergencyContact(String name, String relationship, String phoneNumber) {
-<<<<<<< HEAD
-        
-=======
         this.name = name;
         this.relationship = relationship;
         this. phoneNumber = phoneNumber;
->>>>>>> DeepNPatel
     }
 }
