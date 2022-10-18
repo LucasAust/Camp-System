51b30476908c1e9e667dd0eaa178a4430f4d3144
Deep Patel <dp8@email.sc.edu>
2022-10-18 13:38:28 -0400

    wip

---
+++ Cabin.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Cabin {
+    public String ageRange;
+    public Counselor counselor;
+    public int maxCampers; 
+    public ArrayList<Child> campers = new ArrayList<>();
+    public ArrayList<Cabins> cabins = new ArrayList<>();
+    public Schedule schedule;
+    
+    public Cabin(ageRange, Counselor, maxCampers) {
+
+    }
+
+    public addCounselor() {
+
+    }
+
+    public addCampers() {
+
+    }
+
+    public Schedule getSchedule() {
+        
+    }
+
+}
