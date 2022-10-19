0506db3591a258d822747796cccdd079a3440770
Max Eisenhardt <eisenhm@email.sc.edu>
2022-10-18 22:15:53 -0400

    sessions.json complete

--- GenUUID.java
+++ GenUUID.java
@@ -2,7 +2,7 @@ import java.util.UUID;
 
 public class GenUUID {
     public static void main(String[] args){
-        for(int i=0; i<10; i++){
+        for(int i=0; i<8; i++){
             System.out.println(UUID.randomUUID());
         }
     }
