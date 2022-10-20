5b9bbe0a5b594af5378bbf8e57e70dad1ce6d0fc
LucasAust <89277576+LucasAust@users.noreply.github.com>
2022-10-20 13:39:45 -0400

    folder

--- DataReader.java
+++ DataReader.java
@@ -1,9 +1,16 @@
 
 import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 //Ali Omer
 public class DataReader {
 
+  JSONParser jsonParser = new JSONParser();
   public void saveAllUsers(ArrayList<User> users){
 
   }
