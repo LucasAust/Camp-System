5b34d1ad792847d85fd5c645cc47588c5b1cf93c
Deep Patel <dp8@email.sc.edu>
2022-10-20 14:07:09 -0400

    child

--- Child.java
+++ Child.java
@@ -14,28 +14,6 @@ public class Child {
     public ArrayList<String> medications = new ArrayList<String>();
     public ArrayList<String> emergencyContacts = new ArrayList<String>();
 
-<<<<<<< HEAD
-    public Child(String firstName, String lastName, String age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-
-    }
-
-    public void addAllergies(ArrayList<String> allergies) {
-
-    }
-
-    public void addMedication(ArrayList<String> medications) {
-
-    }
-
-    public void addEmergencyContact(emergencyContact emergencyContact) {
-
-    }
-
-    public void addHealthInfo(healthInfo healthInfo) {
-
-=======
     public Child(String firstName, String lastName, String age)
     {
         this.firstName = firstName;
@@ -51,16 +29,13 @@ public class Child {
     {
         medications.add(medication);
     }
-    public void addEmergencyContact(ArrayList<String> emergencyContacts)
+    public void addEmergencyContact(emergencyContact emergencyContact)
     {
-        emergencyContact contact = new emergencyContact(emergencyContact.name, emergencyContact.relationship, emergencyContact.phoneNumber);
-        for(int i=0;i<3;i++) {
-            emergencyContacts.add(contact);
-        }
+        emergencyContact contact = new emergencyContact(emergencyContact.name,emergencyContact.relationship,emergencyContact.phoneNumber);
+        emergencyContacts.add(contact);
     }
     public void addHealthInfo(healthInfo healthInfo)
     {
         addHealthInfo(healthInfo);
->>>>>>> b6cdbfeb49ada802dff7eb2b69f7da681cc0a153
     }
 }
