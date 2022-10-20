625ce8a5d2b2cd04703e0d97bd2df8badad9f5ac
new user <newuser@Alis-MacBook-Air.local>
2022-10-20 14:25:13 -0400

    UserList

--- Child.java
+++ Child.java
@@ -14,50 +14,25 @@ public class Child {
     public ArrayList<String> medications = new ArrayList<String>();
     public ArrayList<String> emergencyContacts = new ArrayList<String>();
 
-<<<<<<< HEAD
     public Child(String firstName, String lastName, String age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-
-    }
-
-    public void addAllergies(ArrayList<String> allergies) {
-
-    }
-
-    public void addMedication(ArrayList<String> medications) {
-
-    }
-
-    public void addEmergencyContact(emergencyContact emergencyContact) {
-
-    }
-
-    public void addHealthInfo(healthInfo healthInfo) {
-
-=======
-    public Child(String firstName, String lastName, String age)
-    {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
 
-    public void addAllergies(ArrayList<String> allergies)
-    {
+    public void addAllergies(ArrayList<String> allergies) {
         allergies.add(allergy);
     }
-    public void addMedication(ArrayList<String> medications)
-    {
+
+    public void addMedication(ArrayList<String> medications) {
         medications.add(medication);
     }
-    public void addEmergencyContact(emergencyContact emergencyContact)
-    {
+
+    public void addEmergencyContact(emergencyContact emergencyContact) {
         emergencyContacts.add(emergencyContact);
     }
-    public void addHealthInfo(healthInfo healthInfo)
-    {
+
+    public void addHealthInfo(healthInfo healthInfo) {
         addHealthInfo(healthInfo);
->>>>>>> b6cdbfeb49ada802dff7eb2b69f7da681cc0a153
     }
 }
---
+++ UserList.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class UserList {
+  private ArrayList<User> users;
+  private static UserList UserList;
+
+  private UserList() {
+
+  }
+
+  public static UserList getInstance() {
+
+  }
+
+  public boolean addUser(String firstName, String lastName, String username, String email, String password)
+  {
+
+  }
+
+  public ArrayList<User> getUser(User)
+  {
+
+  }
+
+  public editUser(User)
+  {
+
+  }
+
+  public saveUser(User){
+
+  }
+  
+
+}
