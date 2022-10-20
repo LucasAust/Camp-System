7d272477c7e001162b6b74b176d5bd06d3063f75
Max Eisenhardt <eisenhm@email.sc.edu>
2022-10-20 14:32:26 -0400

    counselor

--- Counselor.java
+++ Counselor.java
@@ -4,9 +4,18 @@ public class Counselor {
   public Schedule schedule;
   public emergencyContact emergencyContact;
   public healthInfo healthcare;
+  public Cabin cabin;
 
   public void viewSchdule(Schedule schedule){
 
+  }
+  
+  public void viewCabin()
+  {
+      for(int i=0; i<campers.size(); i++)
+      {
+          System.out.println(campers);
+      }
   }
 
   public void addAlergies(){
