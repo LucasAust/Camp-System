78599a022959188abf7625cbb37bec054c965311
Max Eisenhardt <eisenhm@email.sc.edu>
2022-10-20 14:31:58 -0400

    counselor

--- Counselor.java
+++ Counselor.java
@@ -1,17 +1,12 @@
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class Counselor {
   public Schedule schedule;
-  public ArrayList<String> campers;
   public emergencyContact emergencyContact;
   public healthInfo healthcare;
 
   public void viewSchdule(Schedule schedule){
 
-  }
-
-  public ArrayList<String> viewCampers(){
-    return null;
   }
 
   public void addAlergies(){
