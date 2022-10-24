4e6ba1e5e3a0cb9d8b49afd4a6ad3c6ea0426297
Ali Omer <omerar@email.sc.edu>
2022-10-24 18:48:40 -0400

    registered user

--- Director.java
+++ Director.java
@@ -1,33 +1,33 @@
 import java.rmi.Remote;
 import java.util.ArrayList;
 
-//Ali Omer
-public class Director extends RegisteredUser{
+public class Director extends RegisteredUser {
   public Director(String firstName, String lastName, String userName, String email, String password) {
-    super(firstName, lastName, userName, email, password);  }
+    super(firstName, lastName, userName, email, password);
+  }
 
   private ArrayList<Activity> activities;
 
-public void addActivities(Activity activity){
+  public void addActivities(Activity activity) {
     activities.add(activity);
-}
+  }
 
-public void Remove(Activity activity){
+  public void Remove(Activity activity) {
     activities.remove(activity);
-}
+  }
 
-public void setCounselorSchedule(Schedule schedule){
+  public void setCounselorSchedule(Schedule schedule) {
 
-}
+  }
 
-public void removeChild(Child child){
-}
+  public void removeChild(Child child) {
+  }
 
-public void assignCabins(Cabin cabin){
+  public void assignCabins(Cabin cabin) {
 
-}
+  }
 
-public void addSession(Sessions session){
+  public void addSession(Sessions session) {
 
-}
+  }
 }
