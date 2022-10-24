76ff087e7f0e3e2c3016ec3abf9ab1a5f4f10d57
Deep Patel <dp8@email.sc.edu>
2022-10-23 22:25:32 -0400

    WIP

--- Counselor.java
+++ Counselor.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Counselor{
     protected String firstName;
     protected String lastName;
@@ -5,4 +7,28 @@ public class Counselor{
     protected Schedule schedule;
     protected emergencyContact emergencyContact;
     protected healthInfo healthInfo;
+    protected String allergy;
+    protected ArrayList<String> allergies = new ArrayList<String>();
+    protected ArrayList<emergencyContact> emergencyContacts;
+
+    public void viewSchedule(Schedule schedule) {
+
+    }
+
+    public void addAllergies() {
+        allergies.add(allergy);
+    }
+
+    public void addEmergencyContact(emergencyContact emergencyContact) {
+        emergencyContact contact = new emergencyContact(emergencyContact.name,emergencyContact.relationship,emergencyContact.phoneNumber);
+        emergencyContacts.add(contact);
+    }
+
+    public void viewCabin() {
+
+    }
+
+    public void addHealthInfo(healthInfo healthInfo) {
+        this.healthInfo = healthInfo;
+    }
 }
