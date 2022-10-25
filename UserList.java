2c3bcfb5ec734202c650f0af23a1b6d5995a1701
Ali Omer <omerar@email.sc.edu>
2022-10-25 13:49:58 -0400

    User List

--- UserList.java
+++ UserList.java
@@ -1,1 +1,31 @@
+import java.util.ArrayList;
 
+public class UserList {
+  private static UserList UserList;
+  private ArrayList<User> users;
+
+  private UserList() {
+    users = DataReader.saveAllUsers();
+  }
+
+  public static UserList getInstance() {
+    if (UserList == null) {
+      UserList = new UserList();
+    }
+    return UserList;
+  }
+
+  // Add a User
+  public void addUser(String firstName, String lastName, String username, String email, String password) {
+    users.add(new RegisteredUser(firstName, lastName, username, email, password));
+  }
+
+  public ArrayList<User> getUser() {
+    return users;
+  }
+
+  public void saveUser() {
+    DataWriter.saveAllUsers(users);
+  }
+
+}
