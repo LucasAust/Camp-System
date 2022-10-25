fc9e2a3a0545850c92ab2d0e479bd610b7a56c38
Deep Patel <dp8@email.sc.edu>
2022-10-25 14:04:44 -0400

    wip

--- Director.java
+++ Director.java
@@ -1,34 +1,42 @@
 import java.rmi.Remote;
 import java.util.ArrayList;
 
-public class Director extends RegisteredUser {
+//Ali Omer
+public class Director extends RegisteredUser{
 
-  private ArrayList<Activity> activities;
+private ArrayList<Activity> activities = new ArrayList<Activity>();
+private ArrayList<Sessions> sessions = new ArrayList<Sessions>();
 
   public Director(String firstName, String lastName, String userName, String email, String password) {
     super(firstName, lastName, userName, email, password);
-  }
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.userName = userName;
+    this.email = email;
+    this.password = password;
+  } 
 
-  public void addActivities(Activity activity) {
+public void addActivities(Activity activity){
     activities.add(activity);
-  }
+}
 
-  public void Remove(Activity activity) {
+public void Remove(Activity activity){
     activities.remove(activity);
-  }
+}
 
-  public void setCounselorSchedule(Schedule schedule) {
+public void setCounselorSchedule(Schedule schedule){
 
-  }
+}
 
-  public void removeChild(Child child) {
-  }
+public void removeChild(Child child){
+    child.removeChild(child);
+}
 
-  public void assignCabins(Cabin cabin) {
+public void assignCabins(Cabin cabin){
 
-  }
+}
 
-  public void addSession(Sessions session) {
-
-  }
+public void addSession(Sessions session){
+    sessions.add(session);
+}
 }
