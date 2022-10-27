f4d8481d6520057017c71f9144bfa1aaa3e4e7a8
Deep Patel <dp8@email.sc.edu>
2022-10-27 14:28:17 -0400

    wip

--- Director.java
+++ Director.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
 
-//Ali Omer
 public class Director extends RegisteredUser{
+
+  Sessions sessions2;
+  Cabin cabin;
 
 public ArrayList<Activity> getActivities() {
     return activities;
@@ -31,16 +35,35 @@ public void Remove(Activity activity){
     activities.remove(activity);
 }
 
-public void setCounselorSchedule(Schedule schedule){
+public void setSessions(){
+    for(int i=1;i<10;i++) {
+      System.out.println("Enter theme for Week " + i + ":");
+      Scanner sessionTitleScanner = new Scanner(System.in);
+      String theme = sessionTitleScanner.nextLine();
+      sessions2.title = theme;
+    } 
+}
 
+public void setCabins(){
+  for(int i=1;i<7;i++) {  
+    System.out.println("Set cabin " + i + " age range (enter value in the format '#-#'");
+    Scanner ageRangeScanner = new Scanner(System.in);
+    String ageRange = ageRangeScanner.nextLine();
+    cabin.ageRange = ageRange;
+  }
+}
+
+public void generateSchedule() {
+    for(int i=1;i<10;i++) {
+      int randomActivity = new Random().nextInt(activities.size());
+      System.out.println("-----------------------------");
+      System.out.println("----------Cabin " + i +"------------")
+      System.out.println(activities[randomActivity]);
+    }
 }
 
 public void removeChild(Child child){
     child.removeChild(child);
-}
-
-public void assignCabins(Cabin cabin){
-
 }
 
 public void addSession(Sessions session){
