05cf875a87692049b08e455ebc8e814bd2ba9fd8
Deep Patel <dp8@email.sc.edu>
2022-10-31 18:52:57 -0400

    wip

--- CampUI.java
+++ CampUI.java
@@ -349,7 +349,7 @@ public class CampUI {
                 counselorDisplay();
                 break;
             case 4:
-                DataWriter.saveAllCounselors(CounselorList.getInstance().getCounselors());
+                //DataWriter.saveAllCounselors(CounselorList.getInstance().getCounselors());
                 System.out.println("Thank you for looking at our camp.");
                 logout();
                 break;
@@ -393,10 +393,12 @@ public class CampUI {
                 directorDisplay();
                 break;
             case 3:
-                //edit schedule
+                //generate schedule
                 break;
+            //case 4:
+
             case 4:
-                //add session
+                //create new session
                 boolean bre = false;
                 ArrayList<ArrayList> week = new ArrayList<ArrayList>();
                 ArrayList<Activity> daySchedule = new ArrayList<Activity>();
@@ -452,7 +454,6 @@ public class CampUI {
                         week.add(daySchedule);
                         }
                         cabin.schedule = week;
-
                     }
                 }   
                 DataWriter.saveAllSessions(sessions,cabins);
@@ -472,7 +473,8 @@ public class CampUI {
                 directorDisplay();
                 break;
             case 6:
-                DataWriter.saveAllDirectors(DirectorList.getInstance().getDirectors());
+            //logout
+                //DataWriter.saveAllDirectors(DirectorList.getInstance().getDirectors());
                 System.out.println("Thank you for looking at our camp.");
                 logout();
                 break;
--- Counselor.java
+++ Counselor.java
@@ -1,19 +1,88 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Counselor{
+public class Counselor extends RegisteredUser{
+
+    private Scanner scanner= new Scanner(System.in);
+
+    public static String getGetAge() {
+        return getAge;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public ArrayList<Cabin> getCabins() {
+        return cabins;
+    }
+
+    public emergencyContact getEmergencyContact() {
+        return emergencyContact;
+    }
+
+    public healthInfo getHealthInfo() {
+        return healthInfo;
+    }
+
+    public String getAllergy() {
+        return allergy;
+    }
+
+    public ArrayList<String> getAllergies() {
+        return allergies;
+    }
+
+    public ArrayList<emergencyContact> getEmergencyContacts() {
+        return emergencyContacts;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public static String getAge;
     protected String firstName;
     protected String lastName;
-    protected int age;
+    protected Long age;
     protected Schedule schedule;
-    protected Cabin cabin;
+    protected ArrayList<Cabin> cabins;
     protected emergencyContact emergencyContact;
     protected healthInfo healthInfo;
     protected String allergy;
     protected ArrayList<String> allergies = new ArrayList<String>();
     protected ArrayList<emergencyContact> emergencyContacts;
+    public String username;
+    public String password;
 
-    public void viewSchedule(Schedule schedule) {
-        System.out.print(schedule);
+    public Counselor(String firstName, String lastName, emergencyContact emergencyContact,healthInfo healthInfo,ArrayList<Cabin> cabins, String username, String password)
+    {
+        super(firstName, lastName, username, email, password);
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.emergencyContact=emergencyContact;
+        this.healthInfo=healthInfo;
+        this.cabins=cabins;
+        this.username=username;
+        this.password=password;
     }
 
     public void addAllergies(ArrayList<String> allergies) {
@@ -27,10 +96,43 @@ public class Counselor{
 
     public void viewCabin() {
 
+ 
     }
 
     public void addHealthInfo(healthInfo healthInfo)
     {
         addHealthInfo(healthInfo);
+    }
+    public void getRoster()
+    {
+        ArrayList<Child> roster = new ArrayList<Child>();
+        ArrayList<String> names = new ArrayList<String>();
+        for(int i=0;i<roster.size();i++)
+        {
+            Child child = roster.get(i);
+            String name = child.getFirstName()+" "+child.getLastName();
+            names.add(name);
+        }
+    }
+
+    public void getVitalInfo(ArrayList<Child> roster)
+    {
+        ArrayList<String> vitals = new ArrayList<String>();
+        for(int i=0;i<roster.size();i++)
+        {
+            Child child = roster.get(i);
+            vitals.add(child.getFirstName()+" "+child.getLastName());
+            vitals.add("Allergies: "+child.getAllergies());
+            vitals.add("Emergency Contacts: "+child.getEmergencyContacts());
+            vitals.add("Medical Information: "+child.getHealthInfo());
+        }
+    }
+
+    public void viewSchedule(ArrayList<Schedule> schedule) {
+            ArrayList<String> dailySchedule = new ArrayList<String>();
+            System.out.println("Enter which week number you would like to view your schedule for.");
+            int week = scanner.nextInt();
+            Cabin cabin = cabins.get(week-1);
+            //ArrayList<String> sched = cabin.getSchedule();
     }
 }
