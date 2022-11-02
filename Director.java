01ca2bca6dc8e7b6603a7a40ecdab0f202a8885a
Deep Patel <dp8@email.sc.edu>
2022-11-02 08:55:46 -0400

    wip

--- Director.java
+++ Director.java
@@ -27,37 +27,55 @@ private ArrayList<Sessions> sessions = new ArrayList<Sessions>();
     this.password = password;
   } 
 
-public void addActivities(Activity activity){
+  public String getfirstName() {
+    return firstName;
+  }
+
+  public String getlastName() {
+    return lastName;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+
+public void addActivity(Activity activity){
     activities.add(activity);
 }
-public String getFirstName()
-{
-  return firstName;
-}
-public String getLastName()
-{
-  return lastName;
-}
-public String getUsername()
-{
-  return userName;
-}
-public String getEmail()
-{
-  return email;
-}
-public String getPassword()
-{
-  return password;
-}
 
-public void Remove(Activity activity){
+public void removeActivity(Activity activity){
     activities.remove(activity);
 }
 
-/**
- * set the themes for the seesions
- */
 public void setSessions(){
     for(int i=1;i<10;i++) {
       System.out.println("Enter theme for Week " + i + ":");
@@ -67,9 +85,6 @@ public void setSessions(){
     } 
 }
 
-/**
- * set up cabin assignments
- */
 public void setCabins(){
   for(int i=1;i<7;i++) {  
     System.out.println("Set cabin " + i + " age range (enter value in the format '#-#'");
@@ -79,12 +94,8 @@ public void setCabins(){
   }
 }
 
-/**
- * creates the schedule for each day
- */
 public void generateSchedule() {
     for(int i=1;i<10;i++) {
-      int randomActivity = new Random().nextInt(activities.size());
       System.out.println("-----------------------------");
       System.out.println("            Cabin " + i +"            ");
       for(int j=1;j<8;j++) {
@@ -104,19 +115,22 @@ public void generateSchedule() {
           System.out.println(dinner);
         }
         else {
-        System.out.println(activities.get(randomActivity));
+          if(!activities.isEmpty()) {
+          int randomActivity = new Random().nextInt(activities.size());
+          System.out.println(activities.get(randomActivity));
+          }
         }
       }
-      //System.out.println("-----------------------------");
+      System.out.println("-----------------------------");
     }
 }
  
 public void removeChild(Child child){
+    
     child.removeChild(child);
 }
 
 public void addSession(Sessions session){
     sessions.add(session);
 }
-
 }
