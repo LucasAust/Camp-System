3a87ca685a193ca68dbb52c5630ad162aba4ff48
Deep Patel <deeppp@email.sc.edu>
2022-11-13 23:33:07 -0500

    facade test ...

--- Facade.java
+++ Facade.java
@@ -146,8 +146,22 @@ public class Facade {
     }
 
     @Test
-    public void testSignup(){
-        
+    public void testSignupGuardian(){
+            // Guardian.addUser(firstName, lastName, username, email, password);
+            if (accountType.equalsIgnoreCase("Guardian")) {
+                GuardianList.getInstance().addGuardian("George", "Pickens", "gpickens11", "gpickens11@gmail.com", "steelersnation", child);
+    }
+
+    @Test
+    public void testSignupDirector(){
+    if (accountType.equalsIgnoreCase("Director")) {
+        DirectorList.getInstance().addDirector("Harper", "Smith", "hsmithhsmith", "hsmithhsmith@gmail.com", "dogloverforever234");
+    }
+
+    @Test
+    public void testSignupCounselor(){
+    if (accountType.equalsIgnoreCase("Counselor")) {
+        CounselorList.getInstance().addCounselor("kyle", "korver", "kkshooter", "kkshottooclean", "bballforlife");
     }
 
     /**
@@ -166,11 +180,6 @@ public class Facade {
 
     }
 
-    @Test
-    public void testLogout(){
-
-    }
-
     public void addActivity(Activity activity) {
         activity = new Activity(null, null,null);
         activity.createActivity();
@@ -178,7 +187,8 @@ public class Facade {
 
 @Test
 public void testAddActivity(){
-
+    Activity activity = new Activity("Waterpark", "Having fun at the waterpark with your other campers.", "Charlotte Waterpark");
+    addActivity(activity);
 }
 
     /**
@@ -191,7 +201,20 @@ public void testAddActivity(){
 
 @Test
 public void testAddSession(){
-
+    ArrayList<Cabin> cabins = new ArrayList<Cabin>();
+    emergencyContact emergencyContact = new emergencyContact("Walter White", "Father", "994-232-4685");
+    ArrayList<String> dietaryRestrictions = new ArrayList<String>();
+    ArrayList<String> allergies = new ArrayList<String>();
+    allergies.add("Latex");
+    ArrayList<String> medications = new ArrayList<String>();
+    medications.add("Epi-Pen");
+    healthInfo healthInfo = new healthInfo("CIGNA Group Inc.", "CG48237000747", "Dr. J.R. Smith", dietaryRestrictions, allergies, medications);
+    Child child = new Child("John", "Smith", "11", "johnsmith33", healthInfo, emergencyContact, "2");
+    Counselor counselor = new Counselor("Jeremy", "Frederickson", "jfred007@yahoo.com", emergencyContact, healthInfo, cabins, "JFred", "ctrlaltdlt123!");
+    ArrayList<ArrayList> schedule = new ArrayList<ArrayList>();
+    Cabin cabin = new Cabin("7-10", null, 80, "43857963463", "0071283823", schedule);
+    Sessions sessions = new Sessions("Ninja", "Ninja", 700.00, "Ninja", "07/1/22-10/8/22", cabins);
+    addSession(sessions);
 }
 
 /**
@@ -204,7 +227,7 @@ public void testAddSession(){
 
 @Test
 public void testAddCabin(){
-
+    cabin = new Cabin("7-10", counselor, 80, "23492384", "2123584353", schedule);
 }
 
 /**
@@ -216,20 +239,20 @@ public void testAddCabin(){
         generateSchedule.generateSchedule();
     }
 
-    @Test
-    public void teestGenerateSchedule(){
-
-    }
+    //@Test
+    //public void testGenerateSchedule(){
+    //}
 
     public void removeActivity(Activity activity) {
-
+        //activity.remove(activity);
     }
 
     @Test
     public void testRemoveActivity(){
-
+        Activity activity = new Activity("Tag", "Running around and chasing others.", "Outside in the yard.");
+        removeActivity(activity);
     }
-
+    
     /**
      * Allows user to view schedule
      * @param sessionName name of session to be viewed
@@ -272,11 +295,6 @@ public void testViewSchedule(){
     {
         Counselor.view();
         
-    }
-
-    @Test
-    public void testView(){
-
     }
 
     /**
@@ -341,7 +359,25 @@ public void testViewSchedule(){
 
     @Test
     public void testViewChildren(){
+        ArrayList<Child> childs = new ArrayList<Child>();
+        String firstName = "Charlie";
+        String lastName = "Brown";
+        ArrayList<String> children = registeredUser.getChildren();
+        childs = DataReader.getAllChildren();
+        for (int i = 0; i < children.size(); i++) {
+            System.out.println(children.get(i));
 
+            int x = children.get(i).indexOf(" ");
+            firstName = children.get(i).substring(0, x);
+            lastName = children.get(i).substring(x + 1);
+            for (int j = 0; j < childs.size(); j++) {
+                Child child = childs.get(j);
+                if (child.getFirstName().equals(firstName) && child.getLastName().equals(lastName)) {
+                    System.out.println(child.getSession());
+                    break;
+                }
+            }
+        } 
     }
 
     /**
@@ -352,11 +388,6 @@ public void testViewSchedule(){
 
         guardian.addChild(string);
         DataWriter.saveAllGuardians(GuardianList.getInstance().getGuardians());
-    }
-
-    @Test
-    public void testAddChild(){
-
     }
 
     /**
@@ -386,11 +417,6 @@ public void testViewSchedule(){
         DataWriter.saveAllChildren(children);
     }
 
-    @Test
-    public void testRegisterChild(){
-
-    }
-
     /**
      * @param childFirstName
      * @param childLastName
@@ -409,6 +435,6 @@ public void testViewSchedule(){
 
     @Test
     public void testRegisterChildForSession(){
-        
+
     }
 }
